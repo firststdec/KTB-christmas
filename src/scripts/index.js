63c3ff77ff2b1b22bf48206fa9805e3ed76ebbe4
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2020-12-05 20:41:44 +0700

    feat: finished footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,13 @@
+// eslint-disable-next-line import/extensions
+import '@fortawesome/fontawesome-free/js/all.js'
 // import hero from './modules/hero'
 // import arrowDown from './modules/arrowDown'
+import footerNav from './modules/footerNav'
 
-// const app = () => {
-//   hero()
-//   arrowDown()
-// }
+const app = () => {
+  // hero()
+  // arrowDown()
+  footerNav()
+}
 
-// app()
+app()
